59efb12522496dcd71d893409ce90ed63085b444
PraveenAndra <76590902+PraveenAndra@users.noreply.github.com>
2025-06-06 16:03:21 -0400

    Created Entities for the project
    Designed and created all the entities and relations. Refer to ERD

---
+++ src/main/java/com/project/hotelBookingManagement/entity/User.java
@@ -0,0 +1,40 @@
+package com.project.hotelBookingManagement.entity;
+
+import com.project.hotelBookingManagement.entity.enums.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user_details")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String name;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/project/hotelBookingManagement/entity/enums/BookingStatus.java
@@ -0,0 +1,8 @@
+package com.project.hotelBookingManagement.entity.enums;
+
+public enum BookingStatus {
+
+    RESERVED,
+    CONFIRMED,
+    CANCELLED
+}
